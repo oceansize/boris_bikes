3173e3ca6790cb2475c05411ceead2a646c99d39
Roi Driscoll <roi.driscoll@gmail.com>
2014-05-28 22:36:11 +0100

    Added bones of the class

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,6 +1,17 @@
 class Bike
 
-def broken?
-end
+  def initialize
+    @broken = false
+  end
+
+  def broken?
+    # is the bike broken?
+    @broken
+  end
+
+  def break!
+    # uh-oh, you broke the bike.
+    @broken = true
+  end
 
 end
